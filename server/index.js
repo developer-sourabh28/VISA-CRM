583597ea4635141622af03b4610de3bb45a813a2
vyntrix-solutions <vyntrixsolutions@gmail.com>
2025-05-21 10:54:15 +0530

    fix server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import branchRoutes from './routes/branchRoutes.js'
+import branchRoutes from './router/branchRoutes.js'
 
 // Routes
 import enquiryRoutes from './router/enquiryRoute.js';
